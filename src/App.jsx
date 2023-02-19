19fdf86000ea40f3a85e9125f464e49468d5274d
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2023-02-19 07:13:03 +0100

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
 
+import styles from './styles/App.module.css';
 function App() {
   return (
     <div className="App">
       <Header/>
-      Hello! Ignite
+      
+      <div className={styles.wrapper}>
+        <aside></aside>
+        <main>
+          
+        </main>
+      </div>
     </div>
   )
 }
